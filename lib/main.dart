96b201eefd44a9b3e5b48733b7f6195a16248c1f
Gulab Singh SIkarwar <94665834+GulabSinghSikarwar@users.noreply.github.com>
2023-01-26 10:33:37 +0530

    scaffold added

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: Scaffold(
+        
+      ),
     );
   }
 }
